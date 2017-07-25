aa432c548483295265b2c32ce2f16290900c61c4
tris0020 <tris0020@umn.edu>
2017-07-25 13:04:31 -0500

    Added Sawyer to CLIC

--- VDX+.user.js
+++ VDX+.user.js
@@ -3,7 +3,7 @@
 // @namespace   vdx-plus
 // @description Inserts lending policies into VDX results and adds background colors to alert keywords
 // @include     https://www.mnlinkgateway.org/vdx/zengine*VDXaction*ZSearchDetails*
-// @version     2.6.11
+// @version     2.6.12
 // @require     http://ajax.googleapis.com/ajax/libs/jquery/1.9.1/jquery.min.js
 // @updateURL   https://github.com/Minitex/VDX-Userscripts/raw/master/VDX%2B.user.js
 // @downloadURL https://github.com/Minitex/VDX-Userscripts/raw/master/VDX%2B.user.js
@@ -119,7 +119,8 @@ var availabilityBad =   [
                         ["ON DISPLAY", "- DISPLAY ITEMS DON'T LOAN - CODE 2"],
                         ["WALDORF", "- STORYTIME COLLECTION DOESN'T LOAN - CODE 2"],
 			["MNARCHIVES", "- MNARCHIVES COLLECTION DOESN'T LOAN - CODE 2"],
-			["Reference", "- REFERENCE COLLECTION DOESN'T LOAN - CODE 2"]
+			["Reference", "- REFERENCE COLLECTION DOESN'T LOAN - CODE 2"],
+			["Sawyer", "- SAWYER COLLECTION DOESN'T LOAN - CODE 2"]
                         ];
 
 
