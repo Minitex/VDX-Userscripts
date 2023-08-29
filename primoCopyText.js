480a93a7ea97d0d4b26278cfa2150a80d08883ce
tris0020 <tris0020@umn.edu>
2023-08-29 14:27:06 -0500

    Update primoCopyText.js
    Updated to reflect Primos barcode change

--- primoCopyText.js
+++ primoCopyText.js
@@ -1,8 +1,8 @@
 // ==UserScript==
 // @name         Primo VE Copy Text
 // @namespace    https://minitex.umn.edu/
-// @version      1.0.3
-// @description  An update to Primo made copying text difficult. This adds copy text buttons and disables the click event to collapse when clicked.
+// @version      1.0.4
+// @description  An update to Primo made copying text difficult. This adds copy text buttons and disables the click even to collapse when clicked.
 // @author       Kyle Triska
 // @require      https://code.jquery.com/jquery-3.7.0.min.js
 // @require      https://gist.github.com/raw/2625891/waitForKeyElements.js
@@ -13,21 +13,17 @@
 // @update       https://github.com/Minitex/VDX-Userscripts/raw/master/primoCopyText.js
 // ==/UserScript==
 
-waitForKeyElements('p[role="button"][tabindex="0"]', addButton);
-
+waitForKeyElements('div[ng-if="item.itemFields[3]"] span[role="button"][tabindex="0"], p[role="button"][tabindex="0"]', addButton);
 
 function addButton(jNode) {
-  if(jNode.text().startsWith('Barcode: ') || jNode.text().startsWith('Location/Call number: ')) {
-    var button = $('<button>').text('Copy Text'); // replace 'Copy Text' with your desired button text
-    button.css('margin-left', '5px'); // apply a left margin to the button
-    button.click(function(event) {
-      event.stopPropagation(); // prevent the click event from propagating to parent elements
-      var clone = jNode.clone(); // clone the paragraph node
-      clone.find('button').remove(); // remove the button from the cloned node
-      var splitText = clone.text().split(": "); // extract the text after the ": " from the cloned paragraph text
-      var text = splitText.slice(1).join(": "); // join back all parts from the second element onward
-      GM_setClipboard(text); // copy the text to the clipboard
-    });
-    jNode.append(button); // append the button to the paragraph
-  }
+    if (jNode.text().startsWith('Location/Call number: ') || jNode.closest('div[ng-if="item.itemFields[3]"]').length > 0) {
+        var button = $('<button>').text('Copy Text'); // replace 'Copy Text' with your desired button text
+        button.css('margin-left', '5px'); // apply a left margin to the button
+        button.click(function(event) {
+            event.stopPropagation(); // prevent the click event from propagating to parent elements
+            var text = jNode.text(); // get the text of the targeted node
+            GM_setClipboard(text); // copy the text to the clipboard
+        });
+        jNode.append(button); // append the button directly to jNode
+    }
 }
