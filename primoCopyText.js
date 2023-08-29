8e419428d3a07d792bc7a786b93d16a6b0f9d783
tris0020 <tris0020@umn.edu>
2023-08-29 15:29:20 -0500

    Update primoCopyText.js
    Fixed a few errors

--- primoCopyText.js
+++ primoCopyText.js
@@ -1,7 +1,7 @@
 // ==UserScript==
 // @name         Primo VE Copy Text
 // @namespace    https://minitex.umn.edu/
-// @version      1.0.4
+// @version      1.0.5
 // @description  An update to Primo made copying text difficult. This adds copy text buttons and disables the click even to collapse when clicked.
 // @author       Kyle Triska
 // @require      https://code.jquery.com/jquery-3.7.0.min.js
@@ -16,14 +16,30 @@
 waitForKeyElements('div[ng-if="item.itemFields[3]"] span[role="button"][tabindex="0"], p[role="button"][tabindex="0"]', addButton);
 
 function addButton(jNode) {
-    if (jNode.text().startsWith('Location/Call number: ') || jNode.closest('div[ng-if="item.itemFields[3]"]').length > 0) {
-        var button = $('<button>').text('Copy Text'); // replace 'Copy Text' with your desired button text
-        button.css('margin-left', '5px'); // apply a left margin to the button
-        button.click(function(event) {
-            event.stopPropagation(); // prevent the click event from propagating to parent elements
-            var text = jNode.text(); // get the text of the targeted node
-            GM_setClipboard(text); // copy the text to the clipboard
-        });
-        jNode.append(button); // append the button directly to jNode
+    // Skip if the text starts with "Description:"
+    if (jNode.text().startsWith('Description:')) {
+        return;
     }
+
+    var button = $('<button>').text('Copy Text'); // replace 'Copy Text' with your desired button text
+    button.css('margin-left', '5px'); // apply a left margin to the button
+    button.click(function(event) {
+        event.stopPropagation(); // prevent the click event from propagating to parent elements
+
+        var clone = jNode.clone(); // clone the node to avoid modifying the original
+        clone.find('button').remove(); // remove the button from the cloned node
+
+        var textToCopy;
+
+        if (jNode.text().startsWith('Location/Call number: ')) {
+            var splitText = clone.text().split(": ");
+            textToCopy = splitText.slice(1).join(": "); // join back all parts from the second element onward
+        } else if (jNode.closest('div[ng-if="item.itemFields[3]"]').length > 0) {
+            textToCopy = clone.text(); // use the text from the clone that doesn't include the button
+        }
+
+        GM_setClipboard(textToCopy); // copy the text to the clipboard
+    });
+
+    jNode.append(button); // append the button directly to jNode
 }
